ccb8004f5bc0ae2414847d742aaed703417f9836
samuel deng <mr.dengshuoling@gmail.com>
2014-01-19 17:09:50 +0800

    Add self.dpset to maintain the datapathid for query. Add more DEBUG INFO
    Signed-off-by: samuel deng <mr.dengshuoling@gmail.com>

--- ryu/app/paraTest.py
+++ ryu/app/paraTest.py
@@ -15,14 +15,16 @@
 
 import json
 import logging
+from ryu.lib import ofctl_v1_3
+from ryu.lib import ofctl_v1_0
+import ryu
 
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.controller import Datapath
-from ryu.controller.handler import set_ev_cls, MAIN_DISPATCHER
-from ryu.lib import ofctl_v1_3
+from ryu.controller.handler import set_ev_cls, MAIN_DISPATCHER, HANDSHAKE_DISPATCHER, CONFIG_DISPATCHER
 from ryu.ofproto import ofproto_v1_3
 from ryu.topology.switches import get_switch, get_link, Switch
+from ryu.controller import dpset
 
 LOG = logging.getLogger("DCRouter_Logger")
 __author__ = 'Samuel'
@@ -33,52 +35,71 @@ class DataCenterRouter(app_manager.RyuApp):
         super(DataCenterRouter, self).__init__(*args, **kwargs)
         self.switches = []
         self.links = {}
-
-        # self.switches = get_switch(self)
-        # self.links = get_link(self)
+        self.dpset = {}
 
         LOG.debug("intitial switches of topology")
         LOG.debug(self.switches)
         LOG.debug("initial links of topology")
         LOG.debug(self.links)
+        LOG.debug("initial dpset")
+        LOG.debug(self.dpset)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def topology_change_handler(self, ev):
         self.switches = get_switch(self)
         self.links = get_link(self)
 
+        LOG.debug('\n\n\n')
         LOG.debug('show switches of topology')
         LOG.debug(self.switches)
-        LOG.debug('\n\n\n\n\n\n')
+        LOG.debug('\n\n\n')
         LOG.debug('show links of topology')
         links_json = json.dumps([link.to_dict() for link in self.links])
         LOG.debug(links_json)
+        LOG.debug('\n\n\n')
 
-        # compute the shorthes path and add flow entry to dp.
-        # eg, there should be add a new flow entry.
+        LOG.debug('show switch_foo')
+        switch_foo = Switch(self.switches.pop())
+        LOG.debug(switch_foo.dp)
+        LOG.debug('\n\n\n')
 
-        eg_switch = Switch(self.switches.pop())
+        LOG.debug('show switch_foo.getDPID')
+        LOG.debug(type(switch_foo.dp.getDPID()))
+        switch_foo_dpid = switch_foo.dp.getDPID()
+        LOG.debug(switch_foo_dpid)
+        LOG.debug('\n\n\n')
+
+        LOG.debug('show dpset')
+        LOG.debug(self.dpset)
+        LOG.debug('\n\n\n')
 
         flow = {
-            # default value.
-            'cookie': 0,
-            'cookie_mask': 0,
             'table_id': 0,
-            'idle_timeout': 0,
-            'hard_timeout': 0,
-            'priority': 0,
-            'buffer_id': ofproto_v1_3.OFP_NO_BUFFER,
-            'out_port': ofproto_v1_3.OFPP_ANY,
-            'out_group': ofproto_v1_3.OFPG_ANY,
-            'flags': 0,
-            'match': {},
-            'action': [],
+            'priority': 32768,
+            'match': {
+                "nw_dst": '10.0.0.1' #FIXME in ofctl_v1_3 is ipv4_src
+            },
+            'out_port': 1,
+            'action': [
+                {
+                    "port": 1,
+                    "type": "OUTPUT"
+                },
+            ],
         }
         cmds = {
             'add': ofproto_v1_3.OFPFC_ADD,
             'modif': ofproto_v1_3.OFPFC_MODIFY,
             'del': ofproto_v1_3.OFPFC_DELETE,
-            'modif_s': ofproto_v1_3.OFPFC_MODIFY_STRICT,
-            'del_s': ofproto_v1_3.OFPFC_DELETE_STRICT,
         }
-        ofctl_v1_3.mod_flow_entry(eg_switch.dp, cmds['add'])
+        ofctl_v1_0.mod_flow_entry(self.dpset[switch_foo_dpid], flow ,cmds['add'])
+
+        LOG.debug("succefully execute the mod_flow_entry\n\n\n")
+
+    @set_ev_cls(ryu.controller.dpset.EventDP, HANDSHAKE_DISPATCHER)
+    def dpset_maintainer(self, ev):
+        dp = ev.dp      #<class 'ryu.controller.controller.Datapath'>
+        if ev.enter:
+            self.dpset[str(dp.id)] = dp #<class dp.id :int>
+        else:
+            del self.dpset[str(dp.id)]
