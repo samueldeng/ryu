1f8d2a0d7e73c7a6b5ea4a9ca45ba8fdc9b38869
Samuel Deng <mr.dengshuoling@gmail.com>
2014-03-17 21:35:59 +0800

    FIX the excessd inserted switches BUG.
    refine the code specif
    
    delete some excessed code hints and add some comments.
    
    Signed-off-by: Samuel Deng <mr.dengshuoling@gmail.com>

--- ryu/app/MeshsrGUI_DBDeamon/TopologyPusher.py
+++ ryu/app/MeshsrGUI_DBDeamon/TopologyPusher.py
@@ -1,48 +1,46 @@
+from datetime import time
 import json
 import urllib2
 import MySQLdb
 
 __author__ = 'samuel'
 
-REST_server = 'http://localhost:8080'
-DBhost = 'localhost'
-DBuser = 'root'
-DBpaasswd = '897375'
-DBname = 'meshr'
+REST_SERVER_ADDR = 'http://localhost:8080'
 
-db = MySQLdb.connect(host=DBhost, user=DBuser, passwd=DBpaasswd, db=DBname)
+DBADRESS = 'localhost:3306'
+DBUSER = 'root'
+DBPASSWD = '897375'
+DBNAME = 'meshr'
+
+TOPO_PUSH_PERIOD = 1  # count as sec
+
+db = MySQLdb.connect(host=DBADRESS, user=DBUSER, passwd=DBPASSWD, db=DBNAME)
 cursor = db.cursor()
 
-response_switches = urllib2.urlopen(REST_server + '/v1.0/topology/switches').read()
-switches_JSON = json.loads(response_switches)
+while True:
+    # Get JSONs from RYU controller.
+    response_switches = urllib2.urlopen(REST_SERVER_ADDR + '/v1.0/topology/switches').read()
+    switches_JSON = json.loads(response_switches)
 
-response_links = urllib2.urlopen(REST_server + '/v1.0/topology/links').read()
-links_JSON = json.loads(response_links)
+    response_links = urllib2.urlopen(REST_SERVER_ADDR + '/v1.0/topology/links').read()
+    links_JSON = json.loads(response_links)
 
-for switch in switches_JSON:
-    # FIXME every insert query will be executed into the table, so there must be more than one instance for one switch.
-    sql_sentence = "INSERT INTO meshsr_node ('id', 'node_id', 'x', 'y', 'type', 'des')\
-                    VALUES (NULL, %s, '0', '0', '1', 'None);" % (switch['dpid'])
-    cursor.execute(sql_sentence)
-    # print switch['dpid']
-    # for ports in switch['ports']:
-    #     print ports['name']
-    #     print ports['hw_addr']
-    #     print ports['port_no']
-    #     print ports['dpid']
+    # Delete All the items in the table to avoid the same switch and edge be inserted.
+    cursor.execute("DELETE FROM meshsr_node")
+    cursor.execute("DELETE FROM meshsr_connection")
 
-for link in links_JSON:
-    src = link['src']
-    dst = link['dst']
-    sql_sentence = "INSERT INTO meshsr_connection ('id', `begin_node_id`, `end_node_id`, `type`, `des`) \
-                    VALUES (NULL, %s, %s, '0', '0');" % (src['dpid'], dst['dpid'])
-    cursor.execute(sql_sentence)
-    # print "SRC"
-    # print src['hw_addr']
-    # print src['port_no']
-    # print src['dpid']
+    # Add all the switches and the link to the database.
+    for switch in switches_JSON:
+        sql_sentence = "INSERT INTO meshsr_node ('id', 'node_id', 'x', 'y', 'type', 'des')\
+                        VALUES (NULL, %s, '0', '0', '1', 'None);" % (switch['dpid'])
+        cursor.execute(sql_sentence)
 
-    # print "DST"
-    # print dst['hw_addr']
-    # print dst['port_no']
-    # print dst['dpid']
+    for link in links_JSON:
+        src = link['src']
+        dst = link['dst']
+        sql_sentence = "INSERT INTO meshsr_connection ('id', `begin_node_id`, `end_node_id`, `type`, `des`) \
+                        VALUES (NULL, %s, %s, '0', '0');" % (src['dpid'], dst['dpid'])
+        cursor.execute(sql_sentence)
+
+    #Sleep for a while to avoid high load to SQLServer.
+    time.sleep(TOPO_PUSH_PERIOD)
