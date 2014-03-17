cd9f4d366946dbec68740f063ed5de9b26301187
Samuel Deng <mr.dengshuoling@gmail.com>
2014-03-17 20:30:31 +0800

    finish the basic process logic except a BUG.
    Signed-off-by: Samuel Deng <mr.dengshuoling@gmail.com>

--- ryu/app/MeshsrGUI_DBDeamon/TopologyPusher.py
+++ ryu/app/MeshsrGUI_DBDeamon/TopologyPusher.py
@@ -5,6 +5,13 @@ import MySQLdb
 __author__ = 'samuel'
 
 REST_server = 'http://localhost:8080'
+DBhost = 'localhost'
+DBuser = 'root'
+DBpaasswd = '897375'
+DBname = 'meshr'
+
+db = MySQLdb.connect(host=DBhost, user=DBuser, passwd=DBpaasswd, db=DBname)
+cursor = db.cursor()
 
 response_switches = urllib2.urlopen(REST_server + '/v1.0/topology/switches').read()
 switches_JSON = json.loads(response_switches)
@@ -13,35 +20,29 @@ response_links = urllib2.urlopen(REST_server + '/v1.0/topology/links').read()
 links_JSON = json.loads(response_links)
 
 for switch in switches_JSON:
-    print switch['dpid']
-    for ports in switch['ports']:
-        print ports['name']
-        print ports['hw_addr']
-        print ports['port_no']
-        print ports['dpid']
+    # FIXME every insert query will be executed into the table, so there must be more than one instance for one switch.
+    sql_sentence = "INSERT INTO meshsr_node ('id', 'node_id', 'x', 'y', 'type', 'des')\
+                    VALUES (NULL, %s, '0', '0', '1', 'None);" % (switch['dpid'])
+    cursor.execute(sql_sentence)
+    # print switch['dpid']
+    # for ports in switch['ports']:
+    #     print ports['name']
+    #     print ports['hw_addr']
+    #     print ports['port_no']
+    #     print ports['dpid']
 
 for link in links_JSON:
     src = link['src']
     dst = link['dst']
+    sql_sentence = "INSERT INTO meshsr_connection ('id', `begin_node_id`, `end_node_id`, `type`, `des`) \
+                    VALUES (NULL, %s, %s, '0', '0');" % (src['dpid'], dst['dpid'])
+    cursor.execute(sql_sentence)
+    # print "SRC"
+    # print src['hw_addr']
+    # print src['port_no']
+    # print src['dpid']
 
-    print "SRC"
-    print src['hw_addr']
-    print src['port_no']
-    print src['dpid']
-
-    print "DST"
-    print dst['hw_addr']
-    print dst['port_no']
-    print type(dst['dpid'])
-
-db = MySQLdb.connect(host='localhost', user='root', passwd='897375', db='meshr')
-cursor = db.cursor()
-
-cursor.execute("SELECT * FROM meshsr_connection")
-
-for row in cursor.fetchall():
-    print row
-
-cursor.execute("SELECT * FROM meshsr_node")
-for row in cursor.fetchall():
-    print row
+    # print "DST"
+    # print dst['hw_addr']
+    # print dst['port_no']
+    # print dst['dpid']
