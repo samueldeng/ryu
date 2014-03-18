95a409e36febfac0849518f20377e8b311d17be7
samuel deng <mr.dengshuoling@gmail.com>
2014-03-18 13:27:49 +0800

    Finish v1.
    Signed-off-by: samuel deng <mr.dengshuoling@gmail.com>

--- ryu/app/MeshsrGUI_DBDeamon/TopologyPusher.py
+++ ryu/app/MeshsrGUI_DBDeamon/TopologyPusher.py
@@ -1,5 +1,5 @@
-from datetime import time
 import json
+from time import sleep
 import urllib2
 import MySQLdb
 
@@ -7,18 +7,18 @@ __author__ = 'samuel'
 
 REST_SERVER_ADDR = 'http://localhost:8080'
 
-DBADRESS = 'localhost:3306'
+DBADRESS = 'localhost'
 DBUSER = 'root'
 DBPASSWD = '897375'
 DBNAME = 'meshr'
 
-TOPO_PUSH_PERIOD = 1  # count as sec
+TOPO_PUSH_PERIOD = 5  # count as sec
 
-db = MySQLdb.connect(host=DBADRESS, user=DBUSER, passwd=DBPASSWD, db=DBNAME)
-cursor = db.cursor()
+conn = MySQLdb.connect(host=DBADRESS, user=DBUSER, passwd=DBPASSWD, db=DBNAME)
+cursor = conn.cursor()
 
 while True:
-    # Get JSONs from RYU controller.
+# Get JSONs from RYU controller.
     response_switches = urllib2.urlopen(REST_SERVER_ADDR + '/v1.0/topology/switches').read()
     switches_JSON = json.loads(response_switches)
 
@@ -31,16 +31,17 @@ while True:
 
     # Add all the switches and the link to the database.
     for switch in switches_JSON:
-        sql_sentence = "INSERT INTO meshsr_node ('id', 'node_id', 'x', 'y', 'type', 'des')\
-                        VALUES (NULL, %s, '0', '0', '1', 'None);" % (switch['dpid'])
+        sql_sentence = "INSERT INTO meshsr_node VALUES (NULL, '%s', '0', '0', '1', 'None');" % (switch['dpid'])
         cursor.execute(sql_sentence)
+
 
     for link in links_JSON:
         src = link['src']
         dst = link['dst']
-        sql_sentence = "INSERT INTO meshsr_connection ('id', `begin_node_id`, `end_node_id`, `type`, `des`) \
-                        VALUES (NULL, %s, %s, '0', '0');" % (src['dpid'], dst['dpid'])
+        sql_sentence = "INSERT INTO meshsr_connection VALUES (NULL, '%s', '%s', '0', '0');" % (src['dpid'], dst['dpid'])
+        print sql_sentence
         cursor.execute(sql_sentence)
 
+    conn.commit()
     #Sleep for a while to avoid high load to SQLServer.
-    time.sleep(TOPO_PUSH_PERIOD)
+    sleep(TOPO_PUSH_PERIOD)
