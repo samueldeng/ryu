904f8866c15affb2c3536712da556c7a06376bb4
Samuel Deng <samuel@localhost.localdomain>
2014-03-26 19:47:49 +0800

    v2 finish.
    Signed-off-by: Samuel Deng <samuel@localhost.localdomain>

--- ryu/app/MeshsrGUI_DBDeamon/TopologyPusher.py
+++ ryu/app/MeshsrGUI_DBDeamon/TopologyPusher.py
@@ -17,8 +17,39 @@ TOPO_PUSH_PERIOD = 5  # count as sec
 conn = MySQLdb.connect(host=DBADRESS, user=DBUSER, passwd=DBPASSWD, db=DBNAME)
 cursor = conn.cursor()
 
+dp_cord_x = dict(str, int)
+dp_cord_y = dict(str, int)
+
+dp_cord_x['000000000001'] = 0
+dp_cord_y['000000000001'] = 0
+
+dp_cord_x['000000000002'] = 0
+dp_cord_y['000000000002'] = 0
+
+dp_cord_x['000000000003'] = 0
+dp_cord_y['000000000003'] = 0
+
+dp_cord_x['000000000004'] = 0
+dp_cord_y['000000000004'] = 0
+
+dp_cord_x['000000000005'] = 0
+dp_cord_y['000000000005'] = 0
+
+dp_cord_x['000000000006'] = 0
+dp_cord_y['000000000006'] = 0
+
+dp_cord_x['000000000007'] = 0
+dp_cord_y['000000000007'] = 0
+
+dp_cord_x['000000000008'] = 0
+dp_cord_y['000000000008'] = 0
+
+dp_cord_x['000000000009'] = 0
+dp_cord_y['000000000009'] = 0
+
+
 while True:
-# Get JSONs from RYU controller.
+    # Get JSONs from RYU controller.
     response_switches = urllib2.urlopen(REST_SERVER_ADDR + '/v1.0/topology/switches').read()
     switches_JSON = json.loads(response_switches)
 
@@ -31,15 +62,30 @@ while True:
 
     # Add all the switches and the link to the database.
     for switch in switches_JSON:
-        sql_sentence = "INSERT INTO meshsr_node VALUES (NULL, '%s', '0', '0', '1', 'None');" % (switch['dpid'])
-        cursor.execute(sql_sentence)
+        switch_dpid = switch['dpid']
+        description = str()
+        for ports in switch['ports']:
+            description += ports['name'] + '\n'
+            description += ports['hw_addr'] + '\n'
+            description += ports['port_no'] + '\n'
+            description += ports['dpid'] + '\n'
 
+        x = str(dp_cord_x[switch_dpid])
+        y = str(dp_cord_y[switch_dpid])
+        sql_sentence = "INSERT INTO meshsr_node VALUES (NULL, '%s', '%s', '%s', '%s', '%s');" \
+                       % (switch_dpid, 0, x, y, str(description))
+        cursor.execute(sql_sentence)
 
     for link in links_JSON:
         src = link['src']
         dst = link['dst']
-        sql_sentence = "INSERT INTO meshsr_connection VALUES (NULL, '%s', '%s', '0', '0');" % (src['dpid'], dst['dpid'])
-        print sql_sentence
+
+        src_port = src['port_no']
+        dst_port = dst['port_no']
+        description = src_port + '-->' + dst_port
+
+        sql_sentence = "INSERT INTO meshsr_connection VALUES (NULL, '%s', '%s', '%s', '%s');" \
+                       % (src['dpid'], dst['dpid'], 0, description)
         cursor.execute(sql_sentence)
 
     conn.commit()
--- ryu/app/MeshsrGUI_DBDeamon/meshsr.sql
+++ ryu/app/MeshsrGUI_DBDeamon/meshsr.sql
@@ -1,21 +1,31 @@
--- --------------------------------------------------------
--- 主机:                           58.240.33.185
--- 服务器版本:                        5.5.31-1 - (Debian)
--- 服务器操作系统:                      debian-linux-gnu
--- HeidiSQL 版本:                  8.3.0.4694
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 3.4.11.1deb2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Mar 26, 2014 at 07:28 PM
+-- Server version: 5.5.35
+-- PHP Version: 5.4.4-14+deb7u8
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- 导出 meshr 的数据库结构
-CREATE DATABASE IF NOT EXISTS `meshr` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `meshr`;
+--
+-- Database: `meshr`
+--
 
+-- --------------------------------------------------------
 
--- 导出  表 meshr.meshsr_connection 结构
+--
+-- Table structure for table `meshsr_connection`
+--
+
 CREATE TABLE IF NOT EXISTS `meshsr_connection` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `begin_node_id` int(11) NOT NULL DEFAULT '0',
@@ -23,12 +33,24 @@ CREATE TABLE IF NOT EXISTS `meshsr_connection` (
   `type` int(11) NOT NULL DEFAULT '0',
   `des` varchar(255) NOT NULL DEFAULT '0',
   KEY `ID` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=57 ;
 
--- 数据导出被取消选择。
+--
+-- Dumping data for table `meshsr_connection`
+--
 
+INSERT INTO `meshsr_connection` (`id`, `begin_node_id`, `end_node_id`, `type`, `des`) VALUES
+(53, 10, 13, 0, '00000004-->00000004'),
+(54, 13, 10, 0, '00000004-->00000004'),
+(55, 13, 11, 0, '00000003-->00000003'),
+(56, 11, 13, 0, '00000003-->00000003');
 
--- 导出  表 meshr.meshsr_node 结构
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `meshsr_node`
+--
+
 CREATE TABLE IF NOT EXISTS `meshsr_node` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `node_id` int(11) NOT NULL DEFAULT '0',
@@ -37,9 +59,17 @@ CREATE TABLE IF NOT EXISTS `meshsr_node` (
   `type` int(11) NOT NULL DEFAULT '0',
   `des` varchar(255) NOT NULL DEFAULT '0',
   KEY `id` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=43 ;
 
--- 数据导出被取消选择。
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+--
+-- Dumping data for table `meshsr_node`
+--
+
+INSERT INTO `meshsr_node` (`id`, `node_id`, `x`, `y`, `type`, `des`) VALUES
+(40, 10, 0, 0, 0, 'eth1\n00:0a:35:01:00:01\n00000001\n0000000000000010\neth2\n00:0a:35:01:00:02\n00000002\n0000000000000010\neth3\n00:0a:35:01:00:03\n00000003\n0000000000000010\neth4\n00:0a:35:01:00:04\n00000004\n0000000000000010\n'),
+(41, 11, 0, 0, 0, 'eth1\n00:0a:35:01:01:01\n00000001\n0000000000000011\neth2\n00:0a:35:01:01:02\n00000002\n0000000000000011\neth3\n00:0a:35:01:01:03\n00000003\n0000000000000011\neth4\n00:0a:35:01:01:04\n00000004\n0000000000000011\n'),
+(42, 13, 0, 0, 0, 'eth1\n00:0a:35:01:03:01\n00000001\n0000000000000013\neth2\n00:0a:35:01:03:02\n00000002\n0000000000000013\neth3\n00:0a:35:01:03:03\n00000003\n0000000000000013\neth4\n00:0a:35:01:03:04\n00000004\n0000000000000013\n');
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
