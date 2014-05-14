a0861cee7f295570130295f1c0714c3aa6e853d5
labsvr <labsvr@labsvr.(none)>
2014-05-14 04:10:09 -0500

    add installation_script_ryu_on ubu12.04

---
+++ install_ryu_ubuntu_1204.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install git
+sudo apt-get install python-pip
+sudo apt-get install build-essential
+sudo apt-get install python-dev
+sudo apt-get install libxml2-dev libxslt1-dev
+sudo pip install gevent  gevent-websocket flask
+git clone https://github.com/samueldeng/ryu.git
+cd ryu/
+python setup.py install
+
